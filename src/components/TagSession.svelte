8d0a9f57db8b4c2a40f10365a60a5eac872475f7
Rahul Barwal <rahulbarwal654@gmail.com>
2021-07-04 13:50:24 +0530

    Session tag UI.

---
+++ src/components/TagSession.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let labels: string[];
+  export let selectedLabel: string;
+
+  //#region  Declarations
+  function selectLabel(label: string) {
+    selectedLabel = label;
+  }
+  //#endregion Declarations
+</script>
+
+<main class="border border-gray-400 flex flex-col gap-4 rounded p-6 pt-4">
+  <h4 class="font-semibold text-xl text-indigo-500">Tag this session</h4>
+  <section class="flex gap-4">
+    {#each labels as label}
+      <span
+        class="rounded-xl border py-1 px-4 cursor-pointer hover:bg-indigo-400 hover:border-indigo-400 hover:text-black
+      {label === selectedLabel
+          ? 'bg-indigo-400 border-indigo-400 text-black'
+          : 'bg-transparent'}"
+        on:click={() => selectLabel(label)}
+      >
+        {label}
+      </span>
+    {/each}
+  </section>
+</main>
--- src/layout/Body.svelte
+++ src/layout/Body.svelte
@@ -1,13 +1,18 @@
 <script>
   import Timer from "../components/RunningTimer.svelte";
+  import TagSession from "../components/TagSession.svelte";
+  const sessionLabels = ["Office", "Study", "Personal Project", "Learning"];
 </script>
 
 <section class="text-gray-400 bg-gray-900 body-font">
   <div
     class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
   >
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10 flex justify-center">
-      <Timer  minutes={1} size={'lg'}/>
+    <div
+      class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10 flex flex-col gap-16 justify-center"
+    >
+      <Timer minutes={1} size={"lg"} />
+      <TagSession labels={sessionLabels} />
     </div>
     <div
       class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"
