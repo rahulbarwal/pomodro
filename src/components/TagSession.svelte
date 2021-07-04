0c711e28fd9c6ece9abbbf591e27e19ba3f06b0f
Rahul Barwal <rahulbarwal654@gmail.com>
2021-07-04 22:02:32 +0530

    UI enhancements

--- src/components/TagSession.svelte
+++ src/components/TagSession.svelte
@@ -9,14 +9,14 @@
   //#endregion Declarations
 </script>
 
-<main class="border border-gray-400 flex flex-col gap-4 rounded p-6 pt-4">
-  <h4 class="font-semibold text-xl text-indigo-500">Tag this session</h4>
+<main class="border border-green-400 flex flex-col gap-4 rounded p-6 pt-4">
+  <h4 class="font-semibold text-xl text-green-500">Tag this session</h4>
   <section class="flex gap-4 flex-wrap">
     {#each labels as label}
       <span
-        class="rounded-xl border py-1 px-4 cursor-pointer hover:bg-indigo-400 hover:border-indigo-400 hover:text-black
+        class="rounded-xl border py-1 px-4 cursor-pointer hover:bg-green-500 hover:border-green-500 hover:text-black
       {label === selectedLabel
-          ? 'bg-indigo-400 border-indigo-400 text-black'
+          ? ' bg-green-500 border-green-500  text-black'
           : 'bg-transparent'}"
         on:click={() => selectLabel(label)}
       >
