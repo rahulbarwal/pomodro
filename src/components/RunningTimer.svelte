53fdb9a620dad78af5402f098d1102bda1a30a30
Rahul Barwal <rahulbarwal654@gmail.com>
2021-07-04 22:02:24 +0530

    Timer Running checks

--- src/components/RunningTimer.svelte
+++ src/components/RunningTimer.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
   export let minutes: number = 25;
   export let size: "sm" | "md" | "lg" = "md";
+  const dispatch = createEventDispatcher();
 
-  let seconds = 60;
+  let seconds = 10;
+  let isTimerRunning = false;
+  let intervalRef;
+  minutes -= 1;
 
-//#region Declarations
+  //#region Declarations
   function getSpanClass() {
     let css = "font-semibold ";
     switch (size) {
@@ -22,19 +28,28 @@
   }
 
   function startTimer() {
-    minutes -= 1;
-    const interval = setInterval(() => {
+    isTimerRunning = true;
+    intervalRef = setInterval(() => {
+      dispatch("timerStarted");
       seconds -= 1;
       if (seconds === 0) {
         if (minutes === 0) {
           seconds = 0;
-          clearInterval(interval);
+          clearInterval(intervalRef);
+          dispatch("timerEnded");
+          isTimerRunning = false;
         } else {
           minutes -= 1;
           seconds = 60;
         }
       }
     }, 1000);
+  }
+
+  function pauseTimer() {
+    isTimerRunning = false;
+    clearInterval(intervalRef);
+    dispatch("timerPaused");
   }
 
   function padWithChar(
@@ -56,8 +71,17 @@
 
   //#region Main Execution
   const spanClass = getSpanClass();
-  startTimer();
   //#endregion Main Execution
 </script>
 
-<span class={spanClass}> {padWithChar(minutes)} : {padWithChar(seconds)} </span>
+<section class="flex justify-center">
+  <span class={spanClass}>
+    {padWithChar(minutes)} : {padWithChar(seconds)}
+  </span>
+</section>
+<button
+  class="p-2 rounded-md border-indigo-400 hover:bg-indigo-200 hover:text-black"
+  type="button"
+  on:click={isTimerRunning ? pauseTimer : startTimer}
+  >{isTimerRunning ? "Pause" : "Start"}</button
+>
