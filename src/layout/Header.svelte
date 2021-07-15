fef788b3c4fc56e8966c5e8da2afa34dfa7d82b2
Rahul Barwal <rahulbarwal654@gmail.com>
2021-07-15 21:37:45 +0530

    #1
    1. Remove actionable items when timer is running

--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+  import { onDestroy } from "svelte";
+  import { IsTimerActive } from "../store/timer.state";
+
+  let isTimerActive = false;
+
+  const timerActiveUnSub = IsTimerActive.subscribe(
+    (val) => (isTimerActive = val)
+  );
+
+  onDestroy(() => {
+    timerActiveUnSub();
+  });
+</script>
+
 <header class="text-gray-400 bg-gray-900 body-font">
   <div
     class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
@@ -23,33 +38,38 @@
     <nav
       class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
     >
-      <a
-        class="text-gray-400 mr-5 hover:text-white hover:no-underline no-underline "
-        href="/">App</a
-      >
-      <a
-        class="text-gray-400 mr-5 hover:text-white hover:no-underline no-underline "
-        href="/labels">Labels</a
-      >
-      <a
-        class="mr-5 hover:text-white hover:no-underline no-underline text-gray-400"
-        href="settings">Timer Settings</a
-      >
+      {#if !isTimerActive}
+        <a
+          class="text-gray-400 mr-5 hover:text-white hover:no-underline no-underline "
+          href="/">App</a
+        >
+        <a
+          class="text-gray-400 mr-5 hover:text-white hover:no-underline no-underline "
+          href="/labels">Labels</a
+        >
+        <a
+          class="mr-5 hover:text-white hover:no-underline no-underline text-gray-400"
+          href="settings">Timer Settings</a
+        >
+      {/if}
     </nav>
-    <button
-      class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-      >Button
-      <svg
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        class="w-4 h-4 ml-1"
-        viewBox="0 0 24 24"
+    {#if !isTimerActive}
+      <button
+        class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
       >
-        <path d="M5 12h14M12 5l7 7-7 7" />
-      </svg>
-    </button>
+        Log In
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-4 h-4 ml-1"
+          viewBox="0 0 24 24"
+        >
+          <path d="M5 12h14M12 5l7 7-7 7" />
+        </svg>
+      </button>
+    {/if}
   </div>
 </header>
